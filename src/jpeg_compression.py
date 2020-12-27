a8e7f1cf9c2259d490d1ab5903853a4506c3c57c
vineeths96 <vinsree1996@gmail.com>
2020-12-27 17:09:27 +0530

    Add JPEG Compression

---
+++ src/jpeg_compression.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import os
+import cv2
+
+
+def jpeg(image, path, quality=10):
+    JPEG_PATH = f'../results/{path}/JPEG'
+    os.makedirs(JPEG_PATH, exist_ok=True)
+
+    image_normalized = cv2.normalize(src=image, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+    image_IM_Object = Image.fromarray(image_normalized, 'RGB')
+    image_IM_Object.save(f'{JPEG_PATH}/image_{quality}', "JPEG", quality=quality)
